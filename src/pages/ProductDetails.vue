8f49a0d75684bb8aaaf667f7c19a9c5648912395
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-07-22 23:41:23 +0900

    Working with dynamic path with props and useRoute function.
    the router objet can't be accessed with this.$route anymore. So,
    in the composition API, the url/path can be accessed by allowing the
    router to pass props by adding [props: true] after the component in the
    route js file, like this.
    
    {path: '/random', component: SomeComponent, props: true }
    
    then they can be accessed in the component generated by taking the props
    in the setup(props)
    
    To keep the routing dynamic and responsive, we can add the data in
    computed or use the useRoute() function imported from vue-router.

--- src/pages/ProductDetails.vue
+++ src/pages/ProductDetails.vue
@@ -3,19 +3,45 @@
     <h2>{{ title }}</h2>
     <h3>${{ price}}</h3>
     <p>{{ description}}</p>
+    <router-link to="/products/p2">Product 2</router-link>
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
+import {  inject, computed  } from 'vue';
+import { useRoute } from 'vue-router';
+//the functions above were written to work with composition API. THey are called composebles.
+//the useRoute allows us to use the route.object.
+
 
 export default {
+  props: ['pid'],
   setup() {
-    const title = ref('');
-    const price = ref(null);
-    const description = ref('');
+    const products = inject('products');
+    // const title = ref('');
+    // const price = ref(null);
+    // const description = ref('');
 
-    return { title, price, description };
+
+    //#### another way to work with router is using the useRoute composable ####
+
+    const route = useRoute();
+    console.log(route)
+
+    //creating computed for all these data to keep the page responsive when pressing a button to product 2.
+    const selectedProduct = computed(() => products.value.find(product => product.id === route.params.pid)); 
+    const title = computed(() => selectedProduct.value.title);
+    const price = computed(() => selectedProduct.value.price);
+    const description = computed(() => selectedProduct.value.description);
+    //in the optional API, the router info can be accessed by the 
+    // this.$route
+    //but in the setup() method, there is no this. keyword.
+    // but it can be done by setting the dynamic segment being passed as a props.
+    //{ path: '/:detail', component: Component, props: true}
+    //the :detail dynamic will be catch by props and used in the setup(props)
+    //you can inject the info from the app.vue to the component.
+
+    return { title, price, description, selectedProduct };
   },
 };
 </script>
